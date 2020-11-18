e9fe08baf14192443c86499a15c2af257da51131
Erick Aranibar <aranibarerick@gmail.com>
2020-11-18 04:23:51 +0000

    git.sh

--- seguridad/index.js
+++ seguridad/index.js
@@ -19,9 +19,12 @@ var linkType = c.open(device, filter, bufSize, buffer);
 
 c.setMinBytes && c.setMinBytes(0);
 
+var IPs = [];
+
+
 c.on('packet', function(nbytes, trunc) {
-  console.log("--------------------------------------------------------------------------------------------------------")
-  console.log("Llegó algo", fechas(new Date())) 
+  //console.log("--------------------------------------------------------------------------------------------------------")
+  //console.log("Llegó algo", fechas(new Date())) 
  // console.log('packet: tamaño ' + nbytes + ' bytes, truncado? ' + (trunc ? 'sí' : 'no'));
 
   // raw packet data === buffer.slice(0, nbytes)
@@ -31,29 +34,64 @@ c.on('packet', function(nbytes, trunc) {
     var ret = decoders.Ethernet(buffer);
 
     if (ret.info.type === PROTOCOL.ETHERNET.IPV4) {
-      console.log('Decodificando la IP');
+      //console.log('Decodificando la IP');
 
       ret = decoders.IPV4(buffer, ret.offset);
-      console.log('de: ' + ret.info.srcaddr + ' a ' + ret.info.dstaddr);
+      //console.log('de: ' + ret.info.srcaddr + ' a ' + ret.info.dstaddr);
+
+      actualizarIPs(ret.info.srcaddr);
 
       if (ret.info.protocol === PROTOCOL.IP.TCP) {
         var datalen = ret.info.totallen - ret.hdrlen;
 
-        console.log('Decodificando TCP');
+        //console.log('Decodificando TCP');
 
         ret = decoders.TCP(buffer, ret.offset);
-        console.log(' del puerto: ' + ret.info.srcport + ' a el puerto: ' + ret.info.dstport);
+        //console.log(' del puerto: ' + ret.info.srcport + ' a el puerto: ' + ret.info.dstport);
         datalen -= ret.hdrlen;
         //console.log(buffer.toString('binary', ret.offset, ret.offset + datalen));
       } else if (ret.info.protocol === PROTOCOL.IP.UDP) {
-        console.log('Decodificando UDP');
+        //console.log('Decodificando UDP');
 
         ret = decoders.UDP(buffer, ret.offset);
-        console.log(' del puerto: ' + ret.info.srcport + ' a el puerto: ' + ret.info.dstport);
+        //console.log(' del puerto: ' + ret.info.srcport + ' a el puerto: ' + ret.info.dstport);
        // console.log(buffer.toString('binary', ret.offset, ret.offset + ret.info.length));
       } else
-        console.log('No soporta IPV4: ' + PROTOCOL.IP[ret.info.protocol]);
-    } else
-      console.log('Unsupported Ethertype: ' + PROTOCOL.ETHERNET[ret.info.type]);
+        {
+          //console.log('No soporta IPV4: ' + PROTOCOL.IP[ret.info.protocol]);
+        }
+    } else {
+      //console.log('Unsupported Ethertype: ' + PROTOCOL.ETHERNET[ret.info.type]);
+    }
   }
 });
+
+
+function actualizarIPs (ip) {
+  if(ip!='104.129.131.178')
+  {
+    var incluye = false;
+    IPs.map(a=>
+    {
+      if(a.ip == ip)
+      {
+        incluye = true;
+      }
+    });
+    if(incluye==false)
+    {
+      IPs.push({ip:ip,cant:1});
+    }
+    else
+    {
+      const ind = IPs.findIndex(a=>ip==a.ip);
+      IPs[ind].cant = IPs[ind].cant + 1;
+    }
+  }
+  
+}
+
+setInterval(()=>
+{
+  console.log(IPs);
+},5000)
