1209429953e81cd7bf1e9a9a0887c2d12e395682
Erick Aranibar <aranibarerick@gmail.com>
2020-11-24 03:32:15 +0000

    git.sh

--- seguridad/index.js
+++ seguridad/index.js
@@ -99,7 +99,7 @@ function timeout() {
         IPs.map((a)=>
         {
           console.log(a.cant, IACant);
-          if(a.cant > IACant + 2000)
+          if(a.cant > IACant * 20)
           {
             banear(a.ip);
           }
